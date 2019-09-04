3d001137b4a45c1ad5e152e4942fb083406effc9
Lili Boxer <ninaliboxer@gmail.com>
2019-09-04 15:57:19 -0700

    fetch location by id logic written

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,8 +1,8 @@
 const initialState = {
-
+  
 };
 
-export default function tempReducer(state = initialState, action) {
+export default function homeReducer(state = initialState, action) {
   switch(action.type) {
     default:
       return state;
